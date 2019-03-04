7ed9ff51257897a89034553aa865aef02ce6c775
gyara <me@gyara.moe>
2019-03-04 15:37:03 +0800

    update build script

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 cd ./blog
-cargo web build --release
-cp ./target/wasm32-unknown-unknown/release/blog.js target/wasm32-unknown-unknown/release/blog.wasm ../static
-cp ./static/* ../static
+cargo web deploy --release
+cp ./target/deploy/* ../static
