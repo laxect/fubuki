624d72d78bc2ce2dff4a8b512ddb93c0a0513718
Gyara <me@gyara.moe>
2019-03-24 02:19:13 +0900

    update name

--- blog/src/posts.rs
+++ blog/src/posts.rs
@@ -4,7 +4,7 @@ use yew::services::fetch::{FetchService, FetchTask, Request, Response};
 use yew::{html, Callback, Component, ComponentLink, Html, Renderable, ShouldRender};
 
 #[derive(PatialEq, Clone)]
-pub struct Artcile {
+pub struct Post {
     pub title: String,
     pub time; String,
     pub summary: String,
@@ -12,18 +12,8 @@ pub struct Artcile {
 }
 
 #[derive(PartialEq, Clone)]
-pub struct ArtcileList {
-    pub list: Vec::<Artcile>,
+pub struct PostList {
+    pub list: Vec::<Post>,
     pub page_num: u32,
     pub on_click: Option<Callback<>>
-}
-
-impl Default for ArtcileList {
-    fn default() -> ArtcileList {
-        ArtcileList {
-            list: Vec::new(),
-            page_num: 0,
-            on_click: None,
-        }
-    }
 }
