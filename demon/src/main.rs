06d77078847bb424ac5c7c6c2c17893dace1c3b2
Gyara <me@gyara.moe>
2019-03-25 22:14:46 +0900

    code format

--- demon/src/main.rs
+++ demon/src/main.rs
@@ -1,5 +1,5 @@
+use actix_web::{fs, fs::NamedFile, http::Method, server, App, HttpRequest, Result};
 use std::env;
-use actix_web::{App, fs, server, Result, HttpRequest, http::Method, fs::NamedFile};
 
 fn index(_req: &HttpRequest) -> Result<NamedFile> {
     Ok(NamedFile::open("./static/index.html")?)
@@ -15,14 +15,12 @@ fn main() {
     bind_address.push_str(port.as_str());
     println!("Server listen on {}", bind_address);
 
-    server::new(
-        || App::new()
+    server::new(|| {
+        App::new()
             .resource(r"/", |r| r.method(Method::GET).f(index))
-            .handler(
-                "/",
-                fs::StaticFiles::new("./static")
-                    .unwrap())
-    )
-    .bind(bind_address).unwrap()
+            .handler("/", fs::StaticFiles::new("./static").unwrap())
+    })
+    .bind(bind_address)
+    .unwrap()
     .run();
 }
