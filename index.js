22110a245b42632db7344f1a73e249cc2378edc5
Gyara <me@gyara.moe>
2019-03-27 21:53:27 +0900

    add han css and js

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-import blog from "./blog/Cargo.toml"
+import blog from './blog/Cargo.toml'
+import han_css from './node_modules/han-css/dist/han.css'
+import han from './node_modules/han-css/dist/han'
+
+han.init()
