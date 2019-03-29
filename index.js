d4ab067077498fa4b23d171cf0df6b5c6cfaa1eb
Gyara <me@gyara.moe>
2019-03-29 13:56:06 +0900

    remove highlight

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import blog from './blog/Cargo.toml'
 import han_css from './node_modules/han-css/dist/han.css'
 import main from './main.sass'
-import Prism from 'prismjs'
+import han from './node_modules/han-css/dist/han'
+
+han.init();
