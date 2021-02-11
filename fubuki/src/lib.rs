a5be47399d0d3000760ac0a72c809375ff9766db
gyara <me@gyara.moe>
2021-02-11 23:04:56 +0900

    1.2.0

--- fubuki/src/lib.rs
+++ fubuki/src/lib.rs
@@ -71,8 +71,9 @@ impl Component for Blog {
                 <Content: page=self.page.clone()
                     on_change=on_change/>
                 <footer>
-                    <p>{ "このブログ記事は" }<a href="https://creativecommons.org/licenses/by-nc-sa/3.0/deed.ja">{ "クリエイティブ・コモンズ 表示-継承ライセンス" }</a>{ "の下で利用可能です" }</p>
-                    <p>{ ["ビルドバージョン：", std::env!("CARGO_PKG_VERSION")].concat() }</p>
+                    <p>{ "このブログ記事は" }<a href="https://creativecommons.org/licenses/by-nc-sa/3.0/deed.ja">{ "クリエイティブ・コモンズ 表示-継承ライセンス" }</a>{ "の下で利用可能です。" }</p>
+                    <p>{ "メールアドレス：me at gyara dot moe。" }</p>
+                    <p>{ ["ビルドバージョン：", std::env!("CARGO_PKG_VERSION"), "。"].concat() }</p>
                 </footer>
             </>
         }
