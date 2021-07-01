23ca4d441da7c7b52d87134b95adab8fba1ebf73
gyara <me@gyara.moe>
2021-07-01 23:07:18 +0900

    update deps

--- webpack.config.js
+++ webpack.config.js
@@ -5,48 +5,48 @@ const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
 const dist = path.resolve(__dirname, "dist");
 
 module.exports = {
-    mode: "production",
-    module: {
-        rules: [
-            {
-                test: /\.sass$/,
-                use: [
-                    "style-loader",
-                    "css-loader",
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            implementation: require('sass')
-                        }
-                    }
-                ]
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.sass$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: 'sass-loader',
+            options: {
+              implementation: require('sass')
             }
+          }
         ]
-    },
-    entry: {
-        index: "./index.js"
-    },
-    output: {
-        path: dist,
-        filename: "index.js",
-        publicPath: "/",
-    },
-    devServer: {
-        contentBase: dist,
-        historyApiFallback: true,
-    },
-    plugins: [
-        new CopyPlugin({
-            patterns: [
-                {from: "public"}
-            ]
-        }),
+      }
+    ]
+  },
+  entry: {
+    index: "./index.js"
+  },
+  output: {
+    path: dist,
+    filename: "index.js",
+    publicPath: "/",
+  },
+  devServer: {
+    contentBase: dist,
+    historyApiFallback: true,
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: "public" }
+      ]
+    }),
 
-        new WasmPackPlugin({
-            crateDirectory: "fubuki",
-        }),
-    ],
-    experiments: {
-        asyncWebAssembly: true
-    }
+    new WasmPackPlugin({
+      crateDirectory: "fubuki",
+    }),
+  ],
+  experiments: {
+    asyncWebAssembly: true
+  }
 };
