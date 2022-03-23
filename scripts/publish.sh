06724057381d49b0f3566104b0c7f17353334b11
gyara <me@gyara.moe>
2022-03-23 16:01:03 +0900

    add web mention

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,1 +1,3 @@
-curl https://pubsubhubbub.appspot.com/ -X POST --header "Content-Type: application/x-www-form-urlencoded" -d "hub.mode=publish&&hub.url=https://blog.gyara.moe/atom.xml" -v
+curl https://pubsubhubbub.appspot.com/ --header "Content-Type: application/x-www-form-urlencoded" -d "hub.mode=publish&&hub.url=https://blog.gyara.moe/atom.xml" -v
+echo "\n\n\n======\n\n\n"
+curl "https://webmention.app/check/?url=https://blog.gyara.moe/atom.xml?token=$WEB_MENTION" -X POST -v
