47567f479f8344acebf5497e9896b3b31284ab7c
gyara <me@gyara.moe>
2022-03-23 16:08:07 +0900

    update

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,3 +1,9 @@
 curl https://pubsubhubbub.appspot.com/ --header "Content-Type: application/x-www-form-urlencoded" -d "hub.mode=publish&&hub.url=https://blog.gyara.moe/atom.xml" -v
-echo "\n\n\n======\n\n\n"
-curl "https://webmention.app/check/?url=https://blog.gyara.moe/atom.xml?token=$WEB_MENTION" -X POST -v
+echo "
+
+
+======
+
+
+"
+curl "https://webmention.app/check/?url=https://blog.gyara.moe/atom.xml" -X POST -v
